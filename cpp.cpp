09facde7731a8ef8088ac7c0c1878bf70cf789ba
Henrique Padovani <39744089+padovanih@users.noreply.github.com>
2023-09-14 15:47:45 -0300

    Create cpp.cpp

---
+++ cpp.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function 
+void backspaceStringCompare(string s, string t)
+{
+}
+
+// Driver Code
+int main()
+{
+  // Function Call
+  backspaceStringCompare(s, t);
+}
