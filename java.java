2867dc10ddc437e57e8d018f09d3e1071a5431bd
Henrique Padovani <39744089+padovanih@users.noreply.github.com>
2023-09-14 15:48:02 -0300

    Create java.java

---
+++ java.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    // Function
+    public static void backspaceStringCompare(String s, String t) {
+
+    }
+
+    // Driver Code
+    public static void main(String args[]) {
+        String s;
+        String t;
+
+        // Function Call
+        backspaceStringCompare(s, t);
+    }
+}
